12605e5a5d1dd7202dccd8f9174baa2702708081
Hugh Davenport <hugh@catalyst.net.nz>
2012-06-13 11:10:08 +1200

    Add menu descriptions, add !info command

--- bot.py
+++ bot.py
@@ -9,19 +9,19 @@ from twisted.words.protocols import irc
 orders = {}
 menus = {
     'lbq': [
-        'Quesadilla (needs 3 options)',
-        'Vegetable curry',
-        'Steak & stout pie',
-        'Chicken salad',
-        'Fish & chips',
-        'Chicken sandwich',
-        'Beef burger',
-        'Margherita pizza',
-        'Beer garden pizza',
-        'Smokin\' chicken pizza',
-        'Spicy sausage pizza',
-        'Four wheel drive pizza',
-        'Pizza of the day'
+        [ 'Quesadilla (needs 3 options)' , 'cheese and your choice of 3 fillings in a toasted tortilla with sour chream. Fillings are: smoky chicken, chorizo, salami, jalapenos, tomato, spinach, roasted red peppers, red onion, chipotle sauce, pesto, mozzarella, blue cheese cream cheese, brie, feta. $12' ],
+        [ 'Vegetable curry' , 'Paneer, chickpea and vegetable curry with rice and beer naan. $15' ],
+        [ 'Steak & stout pie' , 'Steak and cheddar pie with craft stout gravy and shoestring fries. $17' ],
+        [ 'Chicken salad' , 'Harissa marinated chicken salad with roasted beetroot, rocket, grilled halloumi and walnuts in a citrus vinaigrette. $16' ],
+        [ 'Fish & chips' , 'Emerson\'s Pilsner battered fish and chips with housemade sauces and salad greens. $19' ],
+        [ 'Open chicken sandwich' , 'spicy marinated chicken breast with melted brie, salad greens, caramelised onions and garlic mayonnaise on sourdough served with shoestring fries and aioli. $18' ],
+        [ 'Beef burger' , '220g beef patty, streaky bacon, onion rings, roasted beetroot and cheddar with beer-battered chunky fries and aiolo. $19' ],
+        [ 'Margherita pizza' , 'fresh basil, cherry tomatoes, mozzarella and tomato sauce. $14' ],
+        [ 'Beer garden pizza' , 'spinach, roasted pumpkin, pinenuts, rocket pesto and feta cheese. $16' ],
+        [ 'Smokin\' chicken pizza' , 'smoked chicken breast, red onions, jalapenos with cream cheese and smoky chipotle BBQ sauce. $18' ],
+        [ 'Spicy sausage pizza' , 'pepperoni, chorizo and mozzarella with sundried tomato pesto. $18' ],
+        [ 'Four wheel drive pizza' , 'peppered steak, field mushrooms, rocket and blue cheese. $19' ],
+        [ 'Pizza of the day' , 'our chef\'s culinary creation. p.o.a' ]
     ]
 }
 
@@ -59,6 +59,7 @@ class Bot(irc.IRCClient):
         if op == 'help':
             self.msg(channel, '!help: show this message.')
             self.msg(channel, '!menu: show the menu.')
+            self.msg(channel, '!info <n>: show info for an item on the menu.')
             self.msg(channel, '!order [<nick>] <n> <special instructions>: order your lunch. `no beetroot` etc can go in `special instructions`')
             self.msg(channel, '!cancel: cancel your order')
             self.msg(channel, '!list: list current lunch orders')
@@ -91,9 +92,9 @@ class Bot(irc.IRCClient):
             orders[username].append((item,special))
             if special:
                 msgAll('%s added a %s, with instructions: %s.' % \
-                    (username, menu[item], special))
+                    (username, menu[item][0], special))
             else:
-                msgAll('%s added a %s.' % (username, menu[item]))
+                msgAll('%s added a %s.' % (username, menu[item][0]))
 
         if op == 'menu':
             if not menu:
@@ -102,8 +103,23 @@ class Bot(irc.IRCClient):
 
             self.msg(channel, 'menu:')
             for i,m in enumerate(menu):
-                self.msg(channel, '%d) %s' % (i,m))
+                self.msg(channel, '%d) %s' % (i,m[0]))
             self.msg(channel, '-- end of menu --');
+
+        if op == 'info':
+            if not menu:
+                self.msg(channel, 'orders are not open.')
+                return
+
+            if len(parts) < 2:
+                self.msg(channel, 'i\'m confused about what you wanted info on.')
+
+            item = maybe_int(parts[1])
+            if item < 0 or item >= len(menu):
+                self.msg(channel, 'that\s not a thing.')
+                return
+
+            self.msg(channel, '%d) %s - %s' % (item, menu[item][0], menu[item][1]))
 
         if op == 'cancel':
             if not menu:
@@ -130,7 +146,7 @@ class Bot(irc.IRCClient):
             for o,n in sorted(by_type.items(), key=lambda x:len(x[1])):
                 instr = o[1] and '(%s) ' % (o[1],) or ''
                 self.msg(channel, '%dx %s %s[%s]' % \
-                    (len(n), menu[o[0]], instr, ','.join(n)))
+                    (len(n), menu[o[0]][0], instr, ','.join(n)))
             self.msg(channel, '-- end of orders --');
 
         if op == 'open':
